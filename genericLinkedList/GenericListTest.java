be8494471610b6282c6f785186eed484c7cd3159
André Marks <andre.lima@azi.com.br>
2025-04-12 19:33:09 -0500

    [add] generic linked list implementation

---
+++ genericLinkedList/GenericListTest.java
@@ -0,0 +1,21 @@
+package genericLinkedList;
+
+public class GenericListTest {
+    public static void main(String[] args) {
+
+        MyLinkedList<String> list = new MyLinkedList<>();
+
+        System.out.println("Creating list: ");
+        list.addLast("Carro");
+        list.addStart("Moto");
+        list.addLast("Avião");
+        list.addStart("Bicicleta");
+        System.out.println(list);
+
+        System.out.println("\nRemoving element at index 3 (Avião): ");
+        list.removeByPosition(0);
+        System.out.println("Updated List: " + list);
+
+        System.out.println("\nVerifying if the list contains a value (Moto): " + list.contains("Moto"));
+    }
+}
---
+++ genericLinkedList/Node.java
@@ -0,0 +1,34 @@
+package genericLinkedList;
+
+import java.util.Objects;
+
+public class Node<T> {
+    private T value;
+    private Node<T> next;
+
+    public Node(T value) {
+        if(value == null)
+            throw new RuntimeException("Value cannot be null!");
+        this.value = value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public boolean hasNext() {
+        return Objects.nonNull(this.next);
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+}
